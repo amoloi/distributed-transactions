b609261ced41c86e4a84ac17c1d9f85ed0fc2cd3
chehh984 <damir.opalka@live.com>
2020-10-06 22:19:47 +0200

    remove target

--- monolith/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into public.customer (id,balance) values ('2fe24185-9f5f-4e3f-a13f-156f798ddc31', 100);
-insert into public.stock (id,amount,name) values ('57aba29f-f0b4-40c0-a56d-c14818a52fd8', 10, 'Rate One');
-insert into public.stock (id,amount,name) values ('d1e1adfc-25a2-4b0b-9ea1-bf8aae6cfa49', 10, 'Rate Two');
